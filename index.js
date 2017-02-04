c295a9f90c913ebee7d002afdc90ac9ce4d3595f
Ori Riner <oririner@gmail.com>
2017-02-04 18:44:39 +0200

    Add output path and composed components support
    Output path can now be configured.
    It is relative to webpack output directory.
    
    Composed components are now parsed and are sent to renderers.
    
    This commit also makes the plugin generate the doc and not the renderer.
    Which actually makes more sense when you think about it.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const process = require('process');
+const reactDocgen = require('react-docgen');
 const ReactDocGenMarkdownRenderer = require('react-docgen-markdown-renderer');
 
 /**
@@ -8,7 +9,11 @@ const ReactDocGenMarkdownRenderer = require('react-docgen-markdown-renderer');
  */
 function ReactDocGenPlugin(options) {
   this.options = Object.assign({}, {
-    renderers: [new ReactDocGenMarkdownRenderer({ componentsBasePath: process.cwd() })]
+    outputPath: '',
+    renderers: [new ReactDocGenMarkdownRenderer({componentsBasePath: process.cwd()})],
+    resolveCompose(file, composingModule) {
+      return path.resolve(path.dirname(file), composingModule);
+    }
   }, options);
 
   if (!this.options.include) {
@@ -16,11 +21,12 @@ function ReactDocGenPlugin(options) {
   }
 }
 
-ReactDocGenPlugin.prototype.apply = function(compiler) {
+ReactDocGenPlugin.prototype.apply = function (compiler) {
   const self = this;
   const components = {};
+  const loaded = {};
 
-  compiler.plugin('emit', function(compilation, done) {
+  compiler.plugin('emit', function (compilation, done) {
     const files = Object.keys(components);
     let remainingFiles = files.length;
 
@@ -35,38 +41,82 @@ ReactDocGenPlugin.prototype.apply = function(compiler) {
       };
     };
 
-    files.forEach((file) => {
-      self.options.renderers.forEach((renderer) => {
-        const componentName = path.basename(file, path.extname(file));
-        const assetName = componentName + renderer.extension;
+    const getComposes = (file) => {
+      const composes = loaded[file].composes;
 
-        // Read the original asset to generate documentation for (it's probably cached)
-        this.inputFileSystem.readFile(file, function(err, componentContent) {
+      return composes.reduce((allComposed, composing) => {
+        const composedPath = self.options.resolveCompose(file, composing);
 
-          try {
-            // Render the documentation for the current component
-            const componentDocumentation = renderer.render(file, componentContent, self.options);
+        // A resolveCompose function should return null/undefined in case it doesn't know how to resolve the path to that module.
+        // Or in the case the composed module doesn't contain a suitable component definition for react-docgen.
+        if(!composedPath) {
+          return allComposed;
+        }
 
-            // Add the documentation file to the compilation assets
-            compilation.assets[assetName] = getDocumentationAsset(componentDocumentation);
+        const composedFile = files.find((file) => file.indexOf(composedPath) >= 0);
 
-            // Check if this is the last file that we've read, after it we can safely let webpack know that we're done
-            remainingFiles--;
-            if (remainingFiles === 0) {
-              done();
-            }
-          }
-          catch (error) {
-            // TODO: notify about the error in the rendering process.
-            done();
-          }
+
+        if(!composedFile || !loaded[composedFile]){
+          throw new Error(`You didn't ignore a composed path for ${composedPath} or you didn't included it in the options.include`);
+        }
+
+        allComposed.push(loaded[composedFile]);
+
+        // In case we're deeply composing
+        if(loaded[composedFile].composes) {
+          allComposed.push(...getComposes(composedFile).map((ast) => Object.assign({}, ast, {
+            componentName: loaded[composedFile].componentName + '.' + ast.componentName
+          })));
+        }
+
+        return allComposed;
+      }, []);
+    };
+
+    const renderDocumentation = () => {
+      Object.keys(loaded).forEach((file) => {
+        // Get all the ASTs that this file composes (including deeply nested composed components)
+        const composesASTs = !!loaded[file].composes ? getComposes(file) : [];
+
+        self.options.renderers.forEach((renderer) => {
+
+          const assetName = path.join(self.options.outputPath, loaded[file].componentName + renderer.extension);
+
+          // Render the documentation for the current component
+          const componentDocumentation = renderer.render(file, loaded[file], composesASTs);
+
+          // Add the documentation file to the compilation assets
+          compilation.assets[assetName] = getDocumentationAsset(componentDocumentation);
         });
+      });
+      done();
+    };
+
+    files.forEach((file) => {
+      // Read the original asset to generate documentation for (it's probably cached)
+      this.inputFileSystem.readFile(file, function (err, componentContent) {
+        try {
+          loaded[file] = reactDocgen.parse(componentContent);
+          loaded[file].componentName = path.basename(file, path.extname(file));
+        }
+        catch(e){
+          throw new Error(`
+          An error has while generating documentation for ${file} in react-docgen.
+          The internal error is
+          ${e.message}`);
+        }
+
+        // Check if this is the last file that we've read, after it we can safely let webpack know that we're done
+        remainingFiles--;
+        if (remainingFiles === 0) {
+          renderDocumentation();
+        }
       });
     });
   });
 
-  compiler.plugin('normal-module-factory', function(nmf) {
-    nmf.plugin('after-resolve', function(data, next) {
+  compiler.plugin('normal-module-factory', function (nmf) {
+    nmf.plugin('after-resolve', function (data, next) {
       const shouldDocumentFile = typeof self.options.include === 'function' ?
         self.options.include(data.resource) :
         self.options.include.test(data.resource);
